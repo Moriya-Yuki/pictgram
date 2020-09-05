c12fae3991adedad7e0defaa04ffe64843b93d1a
Yuki <y.moriya.schoolpartner@gmail.com>
2020-09-05 01:37:48 +0000

    　エラー箇所

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def new
   end
   
-　def create
-　  @user = User.find_by(email: params[:session][:email])
+  def create
+　  user = User.find_by(email: params[:session][:email])
 　  if user && user.authenticate(params[:session][:password])
 　    log_in user
-　    redirect_to root_path, success:'ログインに成功しました'
+　    redirect_to root_path, success: 'ログインに成功しました'
 　  else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
